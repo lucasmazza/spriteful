bba21a59211d0947f37764ae39f08ff5842098cd
Lucas Mazza <lucastmazza@gmail.com>
2013-06-16 20:07:34 -0300

    Improve Sprite object API and documentation

--- lib/spriteful/sprite.rb
+++ lib/spriteful/sprite.rb
@@ -1,20 +1,66 @@
 require 'rmagick'
 
 module Spriteful
+  # Public: the 'Sprite' class is responsible for combining a directory
+  # of images into a single one, and providing the required information
+  # about the related images.
   class Sprite
-    attr_reader :images, :combined_path
+    # Internal: Data structure to represent the images found in the 'source'
+    # directory.
+    Image = Struct.new(:name, :path, :width, :height)
 
+    # Public: returns the path where the sprite will be saved.
+    attr_reader :path
+
+    # Public: returns name of the sprite.
+    attr_reader :name
+
+    # Public: Initialize a Sprite.
+    #
+    # source - the source directory where the sprite images are located.
+    # destination - the destination directory where the sprite should be saved.
     def initialize(source, destination)
-      sprite_name = source.split('/').last
-      @combined_path = "#{File.join(destination, sprite_name)}.png"
-      @images = Magick::ImageList.new(*Dir["#{source}/*png"])
+      source_pattern = File.join(source, '*.png')
+
+      @name = File.basename(source)
+      @path = "#{File.join(destination, name)}.png"
+      @list = Magick::ImageList.new(*Dir[source_pattern])
     end
 
+    # Public: combines the source images into a single one,
+    # storing the combined image into the sprite path.
+    #
+    # Returns the combined 'Image' object.
     def combine!
-      images.each { |image| image.background_color = 'none' }
-      combined = images.append(true)
-      FileUtils.mkdir_p(File.dirname(combined_path))
-      combined.write(combined_path)
+      @list.each { |image| image.background_color = 'none' }
+      combined = @list.append(true)
+      FileUtils.mkdir_p(File.dirname(path))
+      combined.write(path)
+      create_image(combined)
+    end
+
+    # Public: exposes the source images found in the 'source'
+    # directory.
+    #
+    # Yields an 'Image' object on each interation.
+    #
+    # Returns an 'Enumerator' if no block is given.
+    def each_image
+      return to_enum(__method__) unless block_given?
+      @list.each do |image|
+        yield create_image(image)
+      end
+    end
+
+    protected
+    # Internal: wraps a given 'Magick::Image' object into a plain
+    # 'Image' instance, a simple 'Struct' that abstracts the RMagick
+    # API for dealing with image details like height and width.
+    #
+    # Returns an 'Image' instance.
+    def create_image(magick_image)
+      name = File.basename(magick_image.filename)
+      Image.new(name, magick_image.filename, magick_image.columns, magick_image.rows)
     end
   end
 end
--- spec/sprite_spec.rb
+++ spec/sprite_spec.rb
@@ -2,35 +2,42 @@ require 'fileutils'
 require 'spec_helper'
 
 describe Spriteful::Sprite do
-  it 'expose the source images' do
-    sprite = Spriteful::Sprite.new(File.expand_path('spec/fixtures/simple'), File.expand_path('tmp'))
-    first = sprite.images[0]
-    second = sprite.images[1]
+  let(:source) { File.expand_path('spec/fixtures/simple') }
+  let(:destination) { File.expand_path('tmp') }
 
-    expect(File.basename(first.filename)).to eq('blue.png')
-    expect(File.basename(second.filename)).to eq('red.png')
-  end
-
-  it 'exposes the combined image path' do
-    sprite = Spriteful::Sprite.new(File.expand_path('spec/fixtures/simple'), File.expand_path('tmp'))
-    expect(sprite.combined_path).to eq(File.expand_path('tmp/simple.png'))
+  describe '#path' do
+    it 'returns the path where the combined image will be saved' do
+      sprite = Spriteful::Sprite.new(source, destination)
+      expect(sprite.path).to eq(File.expand_path('tmp/simple.png'))
+    end
   end
 
   describe '#combine!' do
-    let(:source) { File.expand_path('spec/fixtures/simple') }
 
     it 'combines the source images into a single image' do
-      sprite = Spriteful::Sprite.new(source, File.expand_path('tmp'))
-      sprite.combine!
-      expect(File.exist?(File.expand_path('tmp/simple.png'))).to be
+      sprite = Spriteful::Sprite.new(source, destination)
+      combined = sprite.combine!
+      expect(File.exist?(combined.path)).to be
     end
 
     it 'creates the destination directory if necessary' do
-      missing_dir = File.expand_path('tmp/missing')
-      FileUtils.rm_rf(missing_dir)
-      sprite = Spriteful::Sprite.new(source, missing_dir)
+      missing_destination = File.expand_path('tmp/missing')
+      FileUtils.rm_rf(missing_destination)
+      sprite = Spriteful::Sprite.new(source, missing_destination)
       sprite.combine!
       expect(File.exist?(File.expand_path('tmp/missing/simple.png'))).to be
+    end
+  end
+
+  describe '#each_image' do
+    it 'yields an object for each image found in the source directory' do
+      sprite = Spriteful::Sprite.new(source, destination)
+      image = sprite.each_image.to_a.first
+
+      expect(image.name).to eq('blue.png')
+      expect(image.width).to eq(10)
+      expect(image.height).to eq(10)
+      expect(image.path).to eq(File.join(source, 'blue.png'))
     end
   end
 end
