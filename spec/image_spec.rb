27c2c41fd48085b917f65278c3fa5d8156c79bc7
Lucas Mazza <lucastmazza@gmail.com>
2013-08-16 22:36:59 -0300

    Remove reference to the Magick namespace.

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Spriteful::Image do
   let(:path) { 'path/to/image.png' }
-  let(:chunky_image) { double('Magick Image', width: 10, height: 5) }
+  let(:chunky_image) { double('ChunkyPNG Image', width: 10, height: 5) }
   subject(:image) { Spriteful::Image.new(chunky_image, path) }
 
   it { expect(image.name).to eq('image.png') }
